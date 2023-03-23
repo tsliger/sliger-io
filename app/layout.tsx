a3931249ce20a9e20c15d13930244ba47f58a844
Thomas Sliger <tsliger@lssu.edu>
2023-03-23 13:45:47 -0400

    fixed issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import dynamic from 'next/dynamic'
 const Navbar = dynamic(() => import('./components/Navbar'))
 const Footer = dynamic(() => import('./components/Footer'))
 import { ChakraProvider } from '@chakra-ui/react'
-import { CacheProvider } from '@chakra-ui/next-js'
 import theme from './misc/theme'
 import { ColorModeScript } from '@chakra-ui/react'
 
@@ -17,14 +16,12 @@ export default function RootLayout({
   return (
       <html lang="en">
         <body>
-          <CacheProvider>
             <ChakraProvider theme={theme}>
               <ColorModeScript initialColorMode={theme.config.initialColorMode} />
               <Navbar />
               {children}
               <Footer />
             </ChakraProvider>
-          </CacheProvider>
         </body>
       </html>
   )
